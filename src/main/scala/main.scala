571b00ca9daf5d171f4a13bf1584224e85a69456
papamitra <papamitra@gmail.com>
2011-10-31 23:08:43 +0900

    display frame

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,11 @@
 
-object Main extends App{
-  println("hello, world")
+import scala.swing._
+
+object Main extends SimpleGUIApplication{
+  def top = new MainFrame{
+    title = "First Swing App"
+    contents = new Button{
+      text = "Click me"
+    }
+  }
 }
